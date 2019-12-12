d0751f1be12049e82f7ec1615222240a46936ca6
stanley-lapinski <stanis.lapinski@gmail.com>
2019-12-12 14:24:44 +0100

    added "How to Play" window

--- src/com/javafx/controllers/MenuController.java
+++ src/com/javafx/controllers/MenuController.java
@@ -1,4 +1,24 @@
 package com.javafx.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class MenuController {
+
+    private RootController rootController;
+
+    @FXML
+    public void playGame(ActionEvent event) throws IOException {
+
+    }
+    @FXML
+    public void openOptions(ActionEvent event) {
+
+    }
+    @FXML
+    public void exit(ActionEvent event) {
+
+    }
 }
--- src/com/javafx/controllers/RootController.java
+++ src/com/javafx/controllers/RootController.java
@@ -1,4 +1,26 @@
 package com.javafx.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
 public class RootController {
+
+    @FXML
+    private StackPane mainStackPane;
+
+    @FXML
+    public void initialize() throws IOException {
+        loadMenuScreen();
+    }
+
+    @FXML
+    public void loadMenuScreen() throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("/com/javafx/screens/MenuScreen.fxml"));
+        mainStackPane.getChildren().add(pane);
+    }
+
 }
