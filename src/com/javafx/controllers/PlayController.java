94f7cba41db22c3dc48492dc02770e80d73176cb
stanley-lapinski <stanis.lapinski@gmail.com>
2019-12-15 15:04:23 +0100

    added game mechanic

--- src/com/javafx/controllers/PlayController.java
+++ src/com/javafx/controllers/PlayController.java
@@ -2,14 +2,31 @@ package com.javafx.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class PlayController {
 
+    @FXML
+    TextField guessNumber;
+    @FXML
+    TextField result;
+
     private RootController rootController;
 
-    @FXML
+    public void checkAction() {
+
+        int theNumber = (int) (Math.random() * 100 + 1);
+        int guess = Integer.parseInt(guessNumber.getText());
+        if (guess < theNumber)
+            result.setText("Your number is too low");
+        else if (guess > theNumber)
+            result.setText("Your number is too high");
+        else
+            result.setText("You are correct!");
+    }
+
     public void backToMenu(ActionEvent event) throws IOException {
         rootController.loadMenuScreen();
     }
