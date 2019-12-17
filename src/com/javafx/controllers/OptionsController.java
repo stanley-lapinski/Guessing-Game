293b422300a9f8dc7d1a99ab7657dece6c3834a7
stanley-lapinski <stanis.lapinski@gmail.com>
2019-12-17 17:05:25 +0100

    added checkboxes and volume sliders to options window

--- src/com/javafx/controllers/OptionsController.java
+++ src/com/javafx/controllers/OptionsController.java
@@ -1,17 +1,36 @@
 package com.javafx.controllers;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ResourceBundle;
 
-public class OptionsController {
+public class OptionsController implements Initializable {
+
+    @FXML
+    CheckBox musicCheckBox, soundCheckBox;
+    @FXML
+    Slider musicVolumeSlider, soundVolumeSlider;
 
     public RootController rootController;
-    public static MediaPlayer backgroundMusicPlayer;
+    public static MediaPlayer backgroundMusicPlayer, correctGuessPlayer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        musicVolumeSlider.setValue(backgroundMusicPlayer.getVolume() * 100);
+        musicVolumeSlider.valueProperty().addListener(observable -> backgroundMusicPlayer.setVolume(musicVolumeSlider.getValue() / 100));
+    }
 
     public static void backgroundMusic() {
         String backgroundMusicPath = "C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic.mp3";
@@ -21,11 +40,25 @@ public class OptionsController {
         backgroundMusicPlayer.play();
     }
 
-    public void pauseMusic(ActionEvent event) {
-        backgroundMusicPlayer.pause();
+    public static void soundEffects() {
+        String correctGuessFile = "C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/correctGuess.mp3";
+        Media correctGuessSound = new Media(new File(correctGuessFile).toURI().toString());
+        correctGuessPlayer = new MediaPlayer(correctGuessSound);
+        correctGuessPlayer.play();
     }
 
-    @FXML
+    public void pauseMusic(ActionEvent event) {
+        boolean musicCheck = musicCheckBox.isSelected();
+        if (!musicCheck) {
+            backgroundMusicPlayer.pause();
+        } else
+            backgroundMusicPlayer.play();
+    }
+
+    public void pauseSound(ActionEvent event) {
+
+    }
+
     public void backToMenu(ActionEvent event) throws IOException {
         rootController.loadMenuScreen();
     }
--- src/com/javafx/controllers/PlayController.java
+++ src/com/javafx/controllers/PlayController.java
@@ -5,18 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.TextField;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-/**
- *  1. Add theme music to all windows
- *  2. Add sounds for correct and wrong guess
- */
 
 public class PlayController implements Initializable {
 
@@ -33,16 +24,8 @@ public class PlayController implements Initializable {
     }
 
     public void checkAction() {
-        /*
-        final java.net.URL resource = getClass().getResource("correctGuess.mp3");
-        final Media media = new Media(resource.toString());
-        final MediaPlayer mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.play(); */
 
-        String correctGuessFile = "C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/correctGuess.mp3";
-        Media correctGuessSound = new Media(new File(correctGuessFile).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(correctGuessSound);
-        mediaPlayer.play();
+        OptionsController.soundEffects();
 
         int theNumber = (int) (Math.random() * 100 + 1);
         int guess = Integer.parseInt(guessNumberInputField.getText());
