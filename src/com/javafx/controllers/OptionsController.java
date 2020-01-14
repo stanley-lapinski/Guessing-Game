d3095507a8522e38150fd9f49a93af9dcebc6d04
stanley-lapinski <stanis.lapinski@gmail.com>
2020-01-14 02:20:17 +0100

    fixed issues concerning save button

--- src/com/javafx/controllers/OptionsController.java
+++ src/com/javafx/controllers/OptionsController.java
@@ -11,12 +11,12 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class OptionsController implements Initializable {
@@ -30,23 +30,21 @@ public class OptionsController implements Initializable {
     @FXML
     private ComboBox<String> changeMusicBox;
 
+    static MediaPlayer backgroundMusicPlayer;
+    private double currentBackgroundVolume;
+    private static String backgroundMusicPath = "file:///C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic1.mp3";
     static int numberRangeFrom = 0, numberRangeTo = 100;
     static double numberOfAllowedGuesses = Double.POSITIVE_INFINITY;
-
     private static String savedRangeFrom = "", savedRangeTo = "", savedAllowedTries = "", savedChangeMusicBox = "Video Game Level";
     private static boolean savedMusicCheckBox = true;
-
-    static MediaPlayer backgroundMusicPlayer;
-    private static String backgroundMusicPath = "C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic1.mp3";
-
+    private static double savedBackgroundVolume = 100.0;
     static Stage warningStage;
     private boolean checkWarning = false;
     private RootController rootController;
-    private double backgroundVolume;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        musicVolumeSlider.setValue(backgroundMusicPlayer.getVolume() * 100);
+        musicVolumeSlider.setValue(savedBackgroundVolume);
         musicVolumeSlider.valueProperty().addListener(observable -> backgroundMusicPlayer.setVolume(musicVolumeSlider.getValue() / 100));
 
         changeMusicBox.getItems().addAll("Video Game Level", "Walk through Meadow", "Discovering New Place");
@@ -84,7 +82,7 @@ public class OptionsController implements Initializable {
     }
 
     public static void backgroundMusic() {
-        Media backgroundMusic = new Media(Paths.get(backgroundMusicPath).toUri().toString());
+        Media backgroundMusic = new Media(backgroundMusicPath);
         backgroundMusicPlayer = new MediaPlayer(backgroundMusic);
         backgroundMusicPlayer.setCycleCount(MediaPlayer.INDEFINITE);
         backgroundMusicPlayer.play();
@@ -98,31 +96,31 @@ public class OptionsController implements Initializable {
     private void changeTheMusicMethod(String songName) {
         switch (songName) {
             case "Video Game Level":
-                backgroundVolume = backgroundMusicPlayer.getVolume();
-                setBackgroundMusicPath("C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic1.mp3");
+                currentBackgroundVolume = backgroundMusicPlayer.getVolume();
+                setBackgroundMusicPath("file:///C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic1.mp3");
                 if (backgroundMusicPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
                     backgroundMusicPlayer.stop();
                     backgroundMusic();
                 }
-                backgroundMusicPlayer.setVolume(backgroundVolume);
+                backgroundMusicPlayer.setVolume(currentBackgroundVolume);
                 break;
             case "Walk through Meadow":
-                backgroundVolume = backgroundMusicPlayer.getVolume();
-                setBackgroundMusicPath("C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic2.mp3");
+                currentBackgroundVolume = backgroundMusicPlayer.getVolume();
+                setBackgroundMusicPath("file:///C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic2.mp3");
                 if (backgroundMusicPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
                     backgroundMusicPlayer.stop();
                     backgroundMusic();
                 }
-                backgroundMusicPlayer.setVolume(backgroundVolume);
+                backgroundMusicPlayer.setVolume(currentBackgroundVolume);
                 break;
             case "Discovering New Place":
-                backgroundVolume = backgroundMusicPlayer.getVolume();
-                setBackgroundMusicPath("C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic3.mp3");
+                currentBackgroundVolume = backgroundMusicPlayer.getVolume();
+                setBackgroundMusicPath("file:///C:/Users/Stanisław/IdeaProjects/GuessingApp_GUI/src/com/javafx/sounds/gameBackgroundMusic3.mp3");
                 if (backgroundMusicPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
                     backgroundMusicPlayer.stop();
                     backgroundMusic();
                 }
-                backgroundMusicPlayer.setVolume(backgroundVolume);
+                backgroundMusicPlayer.setVolume(currentBackgroundVolume);
                 break;
         }
     }
@@ -133,9 +131,9 @@ public class OptionsController implements Initializable {
         if (!musicCheckBox.isSelected())
             backgroundMusicPlayer.pause();
         else {
-            backgroundVolume = backgroundMusicPlayer.getVolume();
+            currentBackgroundVolume = backgroundMusicPlayer.getVolume();
             backgroundMusic();
-            backgroundMusicPlayer.setVolume(backgroundVolume);
+            backgroundMusicPlayer.setVolume(currentBackgroundVolume);
         }
     }
 
@@ -153,6 +151,7 @@ public class OptionsController implements Initializable {
         savedAllowedTries = allowedTriesTextField.getText();
         savedMusicCheckBox = musicCheckBox.isSelected();
         savedChangeMusicBox = changeMusicBox.getValue();
+        savedBackgroundVolume = musicVolumeSlider.getValue();
 
         if (rangeFromTextField.getText().equals("")) { numberRangeFrom = 0; }
         else { numberRangeFrom = Integer.parseInt(savedRangeFrom); }
@@ -166,13 +165,12 @@ public class OptionsController implements Initializable {
 
     @FXML
     private void backToMenuAction() throws IOException {
-
-
         if ((!savedRangeFrom.equals(rangeFromTextField.getText())
                 || !savedRangeTo.equals(rangeToTextField.getText())
                 || !savedAllowedTries.equals(allowedTriesTextField.getText())
-                || !savedMusicCheckBox == musicCheckBox.isSelected()
-                || !savedChangeMusicBox.equals(changeMusicBox.getValue()))
+                || !savedChangeMusicBox.equals(changeMusicBox.getValue())
+                || savedMusicCheckBox != musicCheckBox.isSelected()
+                || savedBackgroundVolume != musicVolumeSlider.getValue())
                 && !checkWarning) {
             checkWarning = true;
             FXMLLoader loader = new FXMLLoader();
@@ -181,18 +179,25 @@ public class OptionsController implements Initializable {
             warningStage = new Stage();
             warningStage.setScene(scene);
             warningStage.initStyle(StageStyle.UNDECORATED);
+            warningStage.initModality(Modality.APPLICATION_MODAL);
             warningStage.show();
         } else {
-
-
             if (savedMusicCheckBox != musicCheckBox.isSelected())
                 pauseMusicExit();
-
             if (!savedChangeMusicBox.equals(changeMusicBox.getValue())) {
-                changeTheMusicMethod(savedChangeMusicBox);
+                if (savedMusicCheckBox != musicCheckBox.isSelected())
+                    pauseMusicExit();
+                else
+                    changeTheMusicMethod(savedChangeMusicBox);
             }
-
-
+            if (savedBackgroundVolume != musicVolumeSlider.getValue()) {
+                if (savedMusicCheckBox != musicCheckBox.isSelected())
+                    pauseMusicExit();
+                else {
+                    backgroundMusicPlayer.setVolume(savedBackgroundVolume);
+                    musicVolumeSlider.setValue(savedBackgroundVolume);
+                }
+            }
             rootController.loadMenuScreen();
         }
     }
